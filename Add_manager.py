40d94d988cfbb83338dc33ee817842a9d451cee7
Surya <suryanarayan209(@gmail.com>
2023-11-02 10:30:21 +0530

    Update password and Add manager functions added

--- Add_manager.py
+++ Add_manager.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 from tkinter import Label, Entry, Button, messagebox
 import mysql.connector
+import Labour, Material_Purchase, Staff_Salary
+import tkinter as tk
+from tkinter import ttk, Label, Entry, Button, messagebox
+import mysql.connector
+import Labour, Material_Purchase, Staff_Salary
 
 # Function to add a new manager and clear input fields
 def add_manager():
@@ -21,7 +26,7 @@ def add_manager():
     existing_record = myc.fetchone()
 
     if existing_record:
-        messagebox.showerror("Error", "Manager already exists!")
+        messagebox.showerror("Error", f"Site number {site} already exists!")
     else:
         # Insert the new manager record into the database
         myc.execute("INSERT INTO login (Site, Username, Password) VALUES (%s, %s, %s)", (site, username, password))
@@ -38,18 +43,18 @@ def add_manager():
 # Create the main window
 root = tk.Tk()
 root.title("Add Manager")
-root.geometry("400x250")
+root.geometry("600x400")  # Set window size to match the Update_password program
 root.configure(bg='#232323')  # Dark background color
 
 # Empty rows and columns for centering
-for i in range(3):
+for i in range(4):
     root.grid_rowconfigure(i, weight=1)
-for i in range(2):
+for i in range(3):
     root.grid_columnconfigure(i, weight=1)
 
 # Heading
 heading = Label(root, text="Add Manager", bg='#232323', fg='white', font=('Arial', 20))
-heading.grid(row=1, column=0, columnspan=2, pady=10)
+heading.grid(row=1, column=1, columnspan=2, pady=10)
 
 # Labels
 site_label = Label(root, text="Site Number:", bg='#232323', fg='white', font=('Arial', 12))
@@ -65,15 +70,49 @@ password_entry = Entry(root, show="*", font=('Arial', 12))  # Show password as a
 add_button = Button(root, text="Add", command=add_manager, font=('Arial', 12))
 
 # Grid layout for labels and entry fields
-site_label.grid(row=2, column=0, padx=10, pady=5, sticky="e")
-site_entry.grid(row=2, column=1, padx=10, pady=5)
-username_label.grid(row=3, column=0, padx=10, pady=5, sticky="e")
-username_entry.grid(row=3, column=1, padx=10,  pady=5)
-password_label.grid(row=4, column=0, padx=10, pady=5, sticky="e")
-password_entry.grid(row=4, column=1, padx=10, pady=5)
+site_label.grid(row=2, column=1, padx=10, pady=5, sticky="e")
+site_entry.grid(row=2, column=2, padx=10, pady=5)
+username_label.grid(row=3, column=1, padx=10, pady=5, sticky="e")
+username_entry.grid(row=3, column=2, padx=10, pady=5)
+password_label.grid(row=4, column=1, padx=10, pady=5, sticky="e")
+password_entry.grid(row=4, column=2, padx=10, pady=5)
 
 # Add button
-add_button.grid(row=5, column=0, columnspan=2, pady=10)
+add_button.grid(row=5, column=1, columnspan=2, pady=10)
+
+def labour():
+    root.destroy()
+    Labour.Labour_page("Techno$pider2099")
+
+def material():
+    root.destroy()
+    Material_Purchase.Material_Purchase_page("Techno$pider2099")
+
+def salary():
+    root.destroy()
+    Staff_Salary.Staff_Salary_page("Techno$pider2099")
+
+# Navigation bar frame
+nav_bar_frame2 = tk.Frame(root, bg="#777777")
+nav_bar_frame2.grid(row=0, column=0, columnspan=3, sticky="news")
+nav_bar_frame = tk.Frame(root, bg="#777777")
+nav_bar_frame.grid(row=0, column=1, columnspan=3, sticky="news")
+
+# Style for navigation bar buttons
+style = tk.ttk.Style()
+style.configure("TButton", font=('Arial', 12))
+
+# Buttons in the navigation bar
+home_button = tk.ttk.Button(nav_bar_frame, text="Local Expenditure", style="TButton")
+manager_button = tk.ttk.Button(nav_bar_frame, text="Material Purchase", style="TButton", command=material)
+director_button = tk.ttk.Button(nav_bar_frame, text="Labour", style="TButton", command=labour)
+exit_button = tk.ttk.Button(nav_bar_frame, text="Staff-Salary", style="TButton", command=salary)
+
+# Grid placement for navigation bar buttons
+home_button.grid(row=0, column=1, padx=10, pady=10)
+manager_button.grid(row=0, column=2, padx=10, pady=10)
+director_button.grid(row=0, column=3, padx=10, pady=10)
+exit_button.grid(row=0, column=4, padx=[10, 100], pady=10)
 
 # Run the tkinter main loop
 root.mainloop()
